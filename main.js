73c79084cb89cad7bab599f9ecd12360dc8295f1
esa344 <hackblock12@gmail.com>
2024-04-05 11:40:59 +0700

    update

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let c = true
+const position = [30, -30, 60, -60, 90, -90]
 function cl() {
   if (c == true) {
-    document.getElementById('content').style.height = '500px'
+    document.getElementById('content').style.height = '400px'
     document.getElementById('content').style.width = '400px'
-    document.getElementById('content').style.marginTop = '90px'
     document.getElementById('bmt').style.display = 'inline'
     document.getElementById('bmw').style.display = 'inline'
     document.getElementById('opening').innerHTML = 'mau ku ajari coding ga?😆'
@@ -12,7 +12,9 @@ function cl() {
 }
 
 function gmw(){
-  document.getElementById('bmt').style.display='none'
+    let x = Math.floor(Math.random() * 6);
+    let y = Math.floor(Math.random() * 6);
+    document.getElementById('bmt').style.margin=`${position[x]}px ${position[y]}px`
 }
 function next(){
   window.location.href='/next/index2.html'
